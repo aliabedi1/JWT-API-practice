0e9025d4852f7016513fbb363c395a268a08f7c5
ali abedi <ali.abedi9731@yahoo.com>
2023-08-10 12:11:58 +0330

    complete register function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
 
 class AuthController extends Controller
 {
     public function __construct() {
         $this->middleware('auth:api' , ['except' => ['login' , 'register']]);
     }
-    public function register(Request $request) {
-        
+    public function register(RegisterRequest $request) {
+        $inputs = $request->all();
+        $inputs['password'] = Hash::make($inputs['password']);
+        return Response::store('user created successfully.',User::create($inputs));
     }
 
     public function login(Request $request) {
