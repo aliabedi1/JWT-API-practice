e27f8c5fcbad8da84fdc9f2b0b5eb346419c6714
ali abedi <ali.abedi9731@yahoo.com>
2023-08-12 09:10:05 +0330

    bug fix of the created middleware that adds a header to all requests to the api

--- app/Http/Middleware/Auth/AddApplciationJsonMiddleware.php
+++ app/Http/Middleware/Auth/AddApplciationJsonMiddleware.php
@@ -16,6 +16,7 @@ class AddApplciationJsonMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request->headers->set('Accept' , 'application/json'));
+        $request->headers->add(['Accept' => 'application/json']);
+        return $next($request);
     }
 }
